52032b5d3556aab71cd2df7a237876e80d162493
MOBILE DEVELOPMENT <mobiledevelopment@DSIs-iMac-3.lan>
2022-10-19 10:45:28 +0100

    new look interface

--- lib/main.dart
+++ lib/main.dart
@@ -14,103 +14,82 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           centerTitle: true,
           title: const Text("iLundary"),
-          actions: const [Icon(Icons.add)],
         ),
         drawer: const Drawer(),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 35.0),
-            const Center(
-              child: Text(
-                "My Lundary Data",
-                style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w900),
-              ),
+        body: Column(children: const [
+          SizedBox(height: 20.5),
+          Center(
+            child: Text(
+              "My Lundary Data",
+              style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.blue),
             ),
-            const YellowBg(),
-            Row(
-              children: const [
-                CompletedOrders(),
-                SizedBox(width: 25),
-                PendingOrders(),
-              ],
-            ),
-            const SizedBox(height: 30.0),
-            const CustomStack()
-          ],
-        ),
+          ),
+          MyOrder(),
+          StatData(),
+          CustomStack(),
+        ]),
       ),
     );
   }
 }
 
-class YellowBg extends StatelessWidget {
-  const YellowBg({Key? key}) : super(key: key);
+class MyOrder extends StatelessWidget {
+  const MyOrder({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(30),
+      margin: const EdgeInsets.only(top: 30, left: 25, right: 25),
       height: 150.0,
-      color: Colors.lightBlueAccent,
-      child: const Center(
-        child: Text(
-          "My Orders",
-          style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w200),
-        ),
-      ),
+      width: double.infinity,
+      color: Colors.blue,
+      child: Center(
+          child: Text(
+        "My Orders".toUpperCase(),
+        style: const TextStyle(color: Colors.white, fontSize: 25),
+      )),
     );
   }
 }
 
-class CompletedOrders extends StatelessWidget {
-  const CompletedOrders({Key? key}) : super(key: key);
+class StatData extends StatelessWidget {
+  const StatData({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 0, left: 30),
-      height: 90.0,
-      width: MediaQuery.of(context).size.width / 2.5,
-      color: Colors.lightBlueAccent,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
-              "41 \n Completed",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w200),
-            ),
-          ],
+    return Row(
+      children: [
+        const SizedBox(
+          width: 25.0,
         ),
-      ),
-    );
-  }
-}
-
-class PendingOrders extends StatelessWidget {
-  const PendingOrders({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 0, right: 30),
-      height: 90.0,
-      width: MediaQuery.of(context).size.width / 2.5,
-      color: Colors.lightBlueAccent,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
-              "20 \n Pending",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w200),
-            ),
-          ],
+        Container(
+          height: 100,
+          width: 150,
+          color: Colors.blue,
+          margin: const EdgeInsets.only(top: 30),
+          child: const Center(
+              child: Text(
+            '42\nCompleted',
+            textAlign: TextAlign.center,
+          )),
         ),
-      ),
+        Container(
+          margin: const EdgeInsets.only(top: 30, left: 40.0),
+          padding: const EdgeInsets.only(left: 15.0),
+          height: 100,
+          width: 150,
+          color: Colors.blue,
+          child: const Center(
+              child: Text(
+            '20\nPending',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white),
+          )),
+        )
+      ],
     );
   }
 }
@@ -123,7 +102,7 @@ class CustomStack extends StatelessWidget {
     return Stack(
       children: [
         Container(
-          margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+          margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 30),
           padding: const EdgeInsets.all(30),
           width: double.infinity,
           color: Colors.lightBlueAccent,
@@ -131,14 +110,14 @@ class CustomStack extends StatelessWidget {
             children: [
               Container(
                 width: double.infinity,
-                color: Colors.greenAccent,
+                color: Colors.orange,
                 padding: const EdgeInsets.all(20),
                 child: const Center(child: Text("Place Order")),
               ),
               const SizedBox(height: 20.0),
               Container(
                 width: double.infinity,
-                color: Colors.red,
+                color: Colors.pink,
                 padding: const EdgeInsets.all(20),
                 child: const Center(child: Text("Exit")),
               ),
