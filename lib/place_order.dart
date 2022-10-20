9f5255b6db14638eaea34fccd020ac0c9089ff0d
MOBILE DEVELOPMENT <mobiledevelopment@DSIs-iMac-3.lan>
2022-10-20 11:58:21 +0100

    New form

--- lib/place_order.dart
+++ lib/place_order.dart
@@ -19,12 +19,64 @@ class PlaceOrder extends StatelessWidget {
       ),
       body: Column(
         children: [
+          const SizedBox(height: 15),
+          const Text(
+            "Enter the details of your Lundary Orders and click to Proceed",
+          ),
+          const SizedBox(height: 15),
+          const Text(
+              "----------------------------------------------------------------------------------------------------"),
+          const SizedBox(height: 15),
+          const SizedBox(height: 25),
+          const Padding(
+            padding: EdgeInsets.only(left: 30, right: 30),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Clothe type",
+                hintText: "e.g T- shirt",
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Clothe Colour",
+                hintText: "e.g red or green",
+              ),
+            ),
+          ),
+          const SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.currency_bitcoin),
+                border: OutlineInputBorder(),
+                labelText: "Price",
+              ),
+            ),
+          ),
+          const SizedBox(height: 15),
+          const Padding(
+            padding: EdgeInsets.only(left: 30, right: 30),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "quantity",
+              ),
+            ),
+          ),
           ElevatedButton(
             onPressed: () {
               Navigator.pop(context);
             },
-            child: const Text("Exit"),
-          )
+            child: const Text("Proceed"),
+          ),
         ],
       ),
     );
